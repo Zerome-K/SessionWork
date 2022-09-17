4f2f9e70b24a9c4af4be842978e19dce827c3c29
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-09-17 12:43:25 +0530

    Update Main.java

--- Applications/CricketGame/Main.java
+++ Applications/CricketGame/Main.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        System.out.println(" -- MATCH BEGINS -- ");
+
+        welcome();
+
         System.out.print("\nENTER OVERS : ");
+
         int overs = in.nextInt();
+
         Ground play = new Ground(overs);
+
         play.start();
+    }
+
+    static void welcome(){
+        System.out.print("\n+-----------------------------------------------+");
+        System.out.println("\n|------------>     MATCH BEGINS    <------------| ");
+        System.out.println("+-----------------------------------------------+");
     }
 }
