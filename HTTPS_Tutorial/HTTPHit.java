7aff1f5e41e29896bd0849d97b9db8bbfddd8626
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-11-03 23:19:24 +0530

    Add files via upload

---
+++ HTTPS_Tutorial/HTTPHit.java
@@ -0,0 +1,36 @@
+package apiTutorial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPHit {
+
+    public void getCall() {
+
+        URL url = null;
+        try {
+            url = new URL(" https://reqres.in/api/users?page=2");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line = reader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+            } else System.out.println("RESPONSE ERROR");
+        } catch(IOException e){
+                System.out.println("NETWORK FAILURE");
+            }
+        }
+
+        public static void main (String[]args){
+            HTTPHit hh = new HTTPHit();
+            hh.getCall();
+        }
+    }
